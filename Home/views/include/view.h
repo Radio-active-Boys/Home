e9f79e2c57464cccde5c2e6926e67fa35802fe67
Ashutosh <ashutoshvishwakarma208@outlook.com>
2023-08-28 20:56:49 +0530

    master upd

--- Home/views/include/view.h
+++ Home/views/include/view.h
@@ -6,15 +6,15 @@
 #include <cmath> 
 #include <stack>
 #include <sstream>
-<<<<<<< HEAD
+
 #include "Node.h"
-=======
+
 
 #include <iostream>
 #include <cctype>
 
 
->>>>>>> 125e0ce4344adeb6380acc8aabc89903470acd31
+
 #include <thread> 
 
 
